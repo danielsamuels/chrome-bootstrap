bf3dfb403e3d5881d7cac8f068459e4ab87808b7
Daniel Samuels <daniel.samuels1@gmail.com>
2013-11-04 16:47:52 +0000

    Changed to use the Foundation grid.

--- background.js
+++ background.js
@@ -5,13 +5,10 @@ var getCurrentIcon = function(toggle) {
   toggle && (current = !current);
   var icon = toggleIcons[+current];
   return icon;
-}
+};
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-
-
-
-  chrome.browserAction.setIcon({path: getCurrentIcon(true)});  
+  chrome.browserAction.setIcon({path: getCurrentIcon(true)});
   chrome.tabs.insertCSS({
     file: 'grid.css'
   });
--- grid.js
+++ grid.js
@@ -1,24 +1,23 @@
 if (document.getElementsByClassName('cb-grid-lines').length){
-    
     document.body.removeChild(document.getElementsByClassName('cb-grid-lines')[0]);
 }
 
 else {
     document.body.innerHTML += '<div class="cb-grid-lines"> \
-      <div class="container"> \
-        <div class="span1"></div> \
-        <div class="span1"></div> \
-        <div class="span1"></div> \
-        <div class="span1"></div> \
-        <div class="span1"></div> \
-        <div class="span1"></div> \
-        <div class="span1"></div> \
-        <div class="span1"></div> \
-        <div class="span1"></div> \
-        <div class="span1"></div> \
-        <div class="span1"></div> \
-        <div class="span1"></div> \
+      <div class="row"> \
+        <div class="small-1 columns"><div class="inner"></div></div> \
+        <div class="small-1 columns"><div class="inner"></div></div> \
+        <div class="small-1 columns"><div class="inner"></div></div> \
+        <div class="small-1 columns"><div class="inner"></div></div> \
+        <div class="small-1 columns"><div class="inner"></div></div> \
+        <div class="small-1 columns"><div class="inner"></div></div> \
+        <div class="small-1 columns"><div class="inner"></div></div> \
+        <div class="small-1 columns"><div class="inner"></div></div> \
+        <div class="small-1 columns"><div class="inner"></div></div> \
+        <div class="small-1 columns"><div class="inner"></div></div> \
+        <div class="small-1 columns"><div class="inner"></div></div> \
+        <div class="small-1 columns"><div class="inner"></div></div> \
       </div> \
     </div>';
-    
+
 }
